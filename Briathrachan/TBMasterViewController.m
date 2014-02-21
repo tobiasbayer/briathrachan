8737bce782198765da2cd8f9c0ca73cef245a18f
Tobias Bayer <tobayer@googlemail.com>
2014-02-21 08:04:26 +0100

    Add basic file parsing

--- Briathrachan/TBMasterViewController.m
+++ Briathrachan/TBMasterViewController.m
@@ -7,106 +7,144 @@
 //
 
 #import "TBMasterViewController.h"
-
 #import "TBDetailViewController.h"
+#import "WordEntry.h"
+#import "BBParser.h"
 
-@interface TBMasterViewController () {
-    NSMutableArray *_objects;
-}
+@interface TBMasterViewController ()
+
+@property (nonatomic, strong) NSArray *index;
+@property (nonatomic, assign) BOOL isSearching;
+@property (nonatomic, strong) NSDictionary *entries;
+@property (nonatomic, strong) NSMutableArray *tempEntries;
+
 @end
 
 @implementation TBMasterViewController
 
-- (void)awakeFromNib
-{
-    [super awakeFromNib];
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
+    [self buildIndex];
+    [self parse];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)parse {
+	@autoreleasepool {
+		NSLog(@"Parsing...");
+		
+		BBParser *parser = [[BBParser alloc] init];
+		_entries = [parser parse:[[NSBundle mainBundle] pathForResource:@"bb-codes" ofType:@"txt"]];
+		
+		[self buildIndex];
+        
+		[self.tableView reloadData];
+		
+		//[_spinner stopAnimating];
+		//_tabBar.selectedItem = [_tabBar.items objectAtIndex:0];
+		//_tabBar.userInteractionEnabled = YES;
+		
+		_tempEntries = [[NSMutableArray alloc] init];
+		
+		for(NSArray *array in [_entries allValues]) {
+			[_tempEntries addObjectsFromArray:array];
+		}
+		
+		NSLog(@"Finished parsing.");
+        
+	}
 }
 
-- (void)insertNewObject:(id)sender
-{
-    if (!_objects) {
-        _objects = [[NSMutableArray alloc] init];
-    }
-    [_objects insertObject:[NSDate date] atIndex:0];
-    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+- (void)buildIndex {
+	NSMutableArray *tempArray = [[NSMutableArray alloc] init];
+	[tempArray addObject:@"A"];
+	[tempArray addObject:@"B"];
+	[tempArray addObject:@"C"];
+	[tempArray addObject:@"D"];
+	[tempArray addObject:@"E"];
+	[tempArray addObject:@"F"];
+	[tempArray addObject:@"G"];
+	[tempArray addObject:@"H"];
+	[tempArray addObject:@"I"];
+	[tempArray addObject:@"L"];
+	[tempArray addObject:@"M"];
+	[tempArray addObject:@"N"];
+	[tempArray addObject:@"O"];
+	[tempArray addObject:@"P"];
+	[tempArray addObject:@"R"];
+	[tempArray addObject:@"S"];
+	[tempArray addObject:@"T"];
+	[tempArray addObject:@"U"];
+	
+	_index = [[NSArray alloc] initWithArray:tempArray];
 }
+
 
 #pragma mark - Table View
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    return 1;
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+	if(_isSearching) {
+		return 1;
+	}
+    return [_index count];
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return _objects.count;
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+	if(_isSearching) {
+		return nil;
+	}
+	return [_index objectAtIndex:section];
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	if(_isSearching) {
+		//return [copyEntries count];
+        return 1;
+	}
+	else {
+		NSString *key = [_index objectAtIndex:section];
+		return [[_entries objectForKey:key] count];
+	}
+}
 
-    NSDate *object = _objects[indexPath.row];
-    cell.textLabel.text = [object description];
+- (UITableViewCell *)tableView:(UITableView *)aTableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [aTableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
+	
+	WordEntry *entry = nil;
+	if(_isSearching) {
+		//entry = [copyEntries objectAtIndex:indexPath.row];
+	}
+	else {
+		NSString *key = [_index objectAtIndex:indexPath.section];
+		entry = [[_entries objectForKey:key] objectAtIndex:indexPath.row];
+	}
+	
+	
+	cell.textLabel.text = entry.original;
+	cell.detailTextLabel.text = entry.translation;
+    
+	cell.selectionStyle = UITableViewCellSelectionStyleNone;
+	
     return cell;
 }
 
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+- (NSArray *)_isSearching:(UITableView *)tableView {
+	if(_isSearching)
+		return nil;
+	
+	return _index;
 }
 
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        [_objects removeObjectAtIndex:indexPath.row];
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }
-}
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     if ([[segue identifier] isEqualToString:@"showDetail"]) {
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        NSDate *object = _objects[indexPath.row];
-        [[segue destinationViewController] setDetailItem:object];
+        
     }
 }
 
