3aa10a073e09cca1d4be63075ad92cb8c2eea1b0
Tobias Bayer <tobayer@googlemail.com>
2014-02-22 09:22:11 +0100

    Update copyright

--- Briathrachan/TBTopLeftAlignedLabel.h
+++ Briathrachan/TBTopLeftAlignedLabel.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2010 Tobias Bayer
+Copyright 2014 Tobias Bayer
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
